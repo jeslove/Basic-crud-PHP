433fcb4a572f5d53f681628f868ddc581f52ba09
Frank Jeslove <jerryjeslove@gmail.com>
2020-01-17 01:29:49 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DEBUG', true); // DEBUG MODE ON
+
+define('DB_NAME', 'DATABASE NAME'); //Databse Name
+
+define('DB_USER', 'YOUR DATABASE USERNAME'); //Databse UserName
+
+define('DB_PASSWORD', 'YOUR DATABASE PASSWORD'); //Databse Password
+
+define('DB_HOST', 'YOUR HOST NAME'); //Databse Host
+
+define('ROOT', 'YOUR DOMIAN NAME'); //set this to the full path of your application (https://www.your domain.com)
+define('HASH_KEY', 'YOUR PASSWORD HASH KEY HERE'); // You can pass your password solted keys here
